3f09125bbeb04bab375157fef3091e6ab5ee2fac
Luke Kaalim <lkaalim@nine.com.au>
2018-08-21 17:33:54 +1000

    Add video resources as valid resource types

--- src/resources/types.js
+++ src/resources/types.js
@@ -1,8 +1,10 @@
 // @flow
 
 import type { ContentResource } from './content/types';
+import type { VideoResource } from './video/types';
 import type { MetaResource } from './meta/types';
 
 export type Resource =
+  | VideoResource
   | MetaResource
   | ContentResource;
