c5d40dd676c19b598f770e6600014c29070d8a73
Luke Kaalim <lkaalim@nine.com.au>
2018-08-21 17:33:01 +1000

    Use curated items function to handle curated items

--- src/resources/content/fetchItem.js
+++ src/resources/content/fetchItem.js
@@ -7,32 +7,25 @@ import type { ItemID, TemplateID } from './types';
 import type { Resource }from '../types';
 
 import { getContentApiDomain } from './api';
+import type { ItemResponse } from './items/types';
+import curatedList from './items/curatedList';
 
 const CuratedListTemplateID = 'd58e6cd8-5f60-40be-ba1d-76a9dad6a4b5';
 
-type CuratedList = {
-  templateId: 'd58e6cd8-5f60-40be-ba1d-76a9dad6a4b5',
-  id: ItemID,
-};
-
-type ItemResponse =
-  | CuratedList;
-
 const getItemUrl = (config: Config, itemId: ItemID): string =>
   `http://${getContentApiDomain(config)}/v1/cms/items/${itemId}?depthLimit=1`;
-
 
 const fetchItem = (config: Config, itemId: ItemID): Promise<Array<Resource>> => (
   fetchJSON(getItemUrl(config, itemId))
     .then((item: ItemResponse) => {
       switch (item.templateId) {
         case CuratedListTemplateID:
-          return [
-            { type: 'list', id: item.id },
+          return curatedList(config, item).then(curatedList => [
+            ...curatedList,
             { type: 'url-meta', url: getItemUrl(config, itemId), response: item, id: uuid() },
-          ];
+          ]);
         default:
-          return [];
+          return [{ type: 'url-meta', url: getItemUrl(config, itemId), response: item, id: uuid() }];
       }
     })
 );
