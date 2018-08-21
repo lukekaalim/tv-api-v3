4720ef8109759711a20a187244609b1442684120
Luke Kaalim <lkaalim@nine.com.au>
2018-08-21 17:34:10 +1000

    Revert handlers to original design

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@
 import { createServer } from 'restify';
 import type { Config } from './types';
 
-//import homeRouteHandler from './routesHandlers/homeRouteHandler';
+import homeRouteHandler from './routesHandlers/homeRouteHandler';
 import rootRouteHandler from './routesHandlers/rootRouteHandler';
 
 const app = (config: Config) => {
   const server = createServer();
 
   server.get('/', rootRouteHandler(config));
-  //server.get('/home', homeRouteHandler);
+  server.get('/home', homeRouteHandler(config));
 
   server.listen(8080, function() {
     console.log('TV_API is running %s %s @ %s', config.environment, config.version, server.url)
   });
 };
 
-app({ environment: 'prod', version: '1.0.0' });
+app({ environment: 'uat', version: '1.0.0' });
--- src/routesHandlers/rootRouteHandler.js
+++ src/routesHandlers/rootRouteHandler.js
@@ -1,45 +1,9 @@
 // @flow
 import type { RouteHandler } from 'restify';
-import type { Resource } from '../resources/types';
 import type { Config } from '../types';
 
-import fetchHomepage from '../resources/content/fetchHomepage';
-
-const findRootPageId = (homepageResource: Array<Resource>) => {
-  const rootPage = homepageResource.find(resource => resource.type === 'page');
-  if (!rootPage) {
-    throw new Error('Failed to resolve with a Useable Homepage');
-  }
-  return rootPage.id;
-};
-
-const zipById = resources => resources.reduce((acc, curr) => ({ ...acc, [(curr.id: string)]: curr }), {});
-
-const countNetworkRequests = resources => (
-  resources
-    .filter(resource => resource.type === 'url-meta')
-    .length
-);
-
-const removeMeta = resources => (
-  resources
-    .filter(resource => resource.type !== 'url-meta' && resource.type !== 'debug-meta')
-);
-
-const formatResponse = (homepageResource: Array<Resource>) => ({
-  debug: {
-    requests: countNetworkRequests(homepageResource),
-  },
-  rootId: findRootPageId(homepageResource),
-  resources: zipById(removeMeta(homepageResource)),
-});
-
 const rootRouteHandler = (config: Config): RouteHandler => (req, res, next) => {
-  fetchHomepage(config)
-    .then(homepageResources => {
-      res.send(formatResponse(homepageResources))
-      res.end();
-    });
+  res.redirect('/home', next);
 };
 
 export default rootRouteHandler;
