d84595a466f3ae1684d9a093d6fe30d34116d34e
Luke Kaalim <luke@kaal.im>
2018-09-10 03:28:58 +1000

    update cms fetchers

--- src/resources/content/fetchHomepage.js
+++ src/resources/content/fetchHomepage.js
@@ -1,18 +1,20 @@
 // @flow
-import uuid from 'uuid/v4';
-import fetchJSON from '../../lib/fetchJson';
+import fetchJSON from '../../lib/fetchJSON';
 
 import type { Config } from '../../types';
-import type { ItemID, TemplateID } from './types';
+import type { CMSItemID } from './types';
 import type { Resource }from '../types';
 
 import { getContentApiDomain } from './api';
 import fetchItem from './fetchItem';
 
+import buildCmsHomePageResource from './buildHomePageResource';
+import { metaRequest } from '../meta/buildMetaResource';
+
 type HomepageResponse = {
-  id: ItemID,
-  templateId: TemplateID,
-  items: Array<{ id: ItemID }>,
+  id: CMSItemID,
+  carousel: { id: CMSItemID },
+  items: Array<{ id: CMSItemID }>,
 };
 
 const flatten = (acc, curr) => [...acc, ...curr];
@@ -20,18 +22,17 @@ const flatten = (acc, curr) => [...acc, ...curr];
 const getHomePageUrl = (config: Config): string =>
   `http://${getContentApiDomain(config)}/v1/cms/items/sitecore/content/9vod/Site/Home%20Page?depthLimit=1`;
 
-const fetchHomepage = (config: Config): Promise<Array<Resource>> => (
-  fetchJSON(getHomePageUrl(config))
-    .then((page: HomepageResponse) => {
-      const itemsIds = page.items.map(item => item.id);
+const fetchHomepage = async (config: Config): Promise<Array<Resource>> => {
+  const homePageUrl = getHomePageUrl(config);
+  const page: HomepageResponse = await fetchJSON(homePageUrl);
+  const itemsIds = [page.carousel.id, ...page.items.map(item => item.id)];
+  const itemsResources = await Promise.all(itemsIds.map(id => fetchItem(config, id)));
 
-      return Promise.all(itemsIds.map(id => fetchItem(config, id)))
-        .then(itemsResources => [
-          ...itemsResources.reduce(flatten, []),
-          { type: 'url-meta', url: getHomePageUrl(config), response: page, id: uuid() },
-          { type: 'page', id: page.id, itemsIds },
-        ]);
-    })
-);
+  return [
+    ...itemsResources.reduce(flatten, []),
+    metaRequest(getHomePageUrl(config)),
+    buildCmsHomePageResource(page.id, itemsIds),
+  ];
+};
 
 export default fetchHomepage;
--- src/resources/content/fetchItem.js
+++ src/resources/content/fetchItem.js
@@ -1,33 +1,51 @@
 // @flow
 import uuid from 'uuid/v4';
-import fetchJSON from '../../lib/fetchJson';
+import fetchJSON from '../../lib/fetchJSON';
 
 import type { Config } from '../../types';
-import type { ItemID, TemplateID } from './types';
+import type { CMSItemID } from './types';
 import type { Resource }from '../types';
+import type { ItemResponse } from './items/types';
 
 import { getContentApiDomain } from './api';
-import type { ItemResponse } from './items/types';
+import { metaRequest } from '../meta/buildMetaResource';
 import curatedList from './items/curatedList';
+import autoList from './items/autoList';
 
-const CuratedListTemplateID = 'd58e6cd8-5f60-40be-ba1d-76a9dad6a4b5';
+const curatedListTemplateId = 'd58e6cd8-5f60-40be-ba1d-76a9dad6a4b5';
+const autoListTemplateId = 'f0f56130-e9d6-43e1-8526-cdf40f5fce73';
+const curatedLinkListTemplateId = '0c5effc7-8e22-471b-bf68-67986887239c';
+const carouselTemplateId = '620aaf06-8c32-4644-b283-58ef9c3b7bbe';
 
-const getItemUrl = (config: Config, itemId: ItemID): string =>
+const getItemUrl = (config: Config, itemId: CMSItemID): string =>
   `http://${getContentApiDomain(config)}/v1/cms/items/${itemId}?depthLimit=1`;
 
-const fetchItem = (config: Config, itemId: ItemID): Promise<Array<Resource>> => (
-  fetchJSON(getItemUrl(config, itemId))
-    .then((item: ItemResponse) => {
-      switch (item.templateId) {
-        case CuratedListTemplateID:
-          return curatedList(config, item).then(curatedList => [
-            ...curatedList,
-            { type: 'url-meta', url: getItemUrl(config, itemId), response: item, id: uuid() },
-          ]);
-        default:
-          return [{ type: 'url-meta', url: getItemUrl(config, itemId), response: item, id: uuid() }];
-      }
-    })
-);
+const fetchItem = async (config: Config, itemId: CMSItemID): Promise<Array<Resource>> => {
+  const itemUrl = getItemUrl(config, itemId);
+  const item: ItemResponse = await fetchJSON(itemUrl);
+  switch (item.templateId) {
+    case curatedListTemplateId:
+      return [
+        ...(await curatedList(config, item)),
+        metaRequest(itemUrl),
+      ];
+    case autoListTemplateId:
+      return [
+        ...(await autoList(config, item)),
+        metaRequest(itemUrl),
+      ];
+    case curatedLinkListTemplateId:
+      return [
+        metaRequest(itemUrl),
+      ];
+    case carouselTemplateId:
+      console.log('carouselScream');
+      return [
+        metaRequest(itemUrl),
+      ]
+    default:
+      return [metaRequest(itemUrl)];
+  }
+};
 
 export default fetchItem;
