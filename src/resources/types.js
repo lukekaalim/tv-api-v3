de2b06d99f0d52d22e1346187e36f549ca7278c3
Luke Kaalim <luke@kaal.im>
2018-09-10 03:27:23 +1000

    update type references

--- src/resources/types.js
+++ src/resources/types.js
@@ -1,10 +1,10 @@
 // @flow
 
-import type { ContentResource } from './content/types';
-import type { VideoResource } from './video/types';
+import type { CMSResource } from './content/types';
+import type { VMSResource } from './video/types';
 import type { MetaResource } from './meta/types';
 
 export type Resource =
-  | VideoResource
+  | VMSResource
   | MetaResource
-  | ContentResource;
+  | CMSResource;
