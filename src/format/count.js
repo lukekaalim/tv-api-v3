07e7479152e5f601b4f51df5fb4d12206314d3f2
Luke Kaalim <luke@kaal.im>
2018-09-10 03:26:24 +1000

    remove old random formatting tools

--- src/format/count.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import type { Resource } from '../resources/types';
-
-export const countType = (resources: Array<Resource>, type: $PropertyType<Resource, 'type'>) => (
-  resources
-    .filter(resource => resource.type === type)
-    .length
-);
--- src/format/filter.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import type { Resource } from '../resources/types';
-
-export const filterType = (resources: Array<Resource>, type: $PropertyType<Resource, 'type'>) => (
-  resources
-    .filter(resource => resource.type !== type)
-);
