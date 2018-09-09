8f8198d23522aeee4172fcde6d28449399409fd8
Luke Kaalim <luke@kaal.im>
2018-09-10 03:26:51 +1000

    simplify home handler

--- src/routesHandlers/homeRouteHandler.js
+++ src/routesHandlers/homeRouteHandler.js
@@ -3,34 +3,27 @@ import type { RouteHandler } from 'restify';
 import type { Resource } from '../resources/types';
 import type { Config } from '../types';
 
-import type { UrlMetaResource } from '../resources/meta/types';
-
-import { zipByType } from '../format/zip';
-import { countType } from '../format/count';
-import { filterType } from '../format/filter';
-import { findType } from '../format/find';
-
 import fetchHomepage from '../resources/content/fetchHomepage';
 
-const formatResponse = (homepageResource: Array<Resource>) => ({
-  debug: {
-    requests: countType(homepageResource, 'url-meta'),
-    requestBodySizeKiloBytes: Math.ceil(homepageResource
-      .map(resource => resource.type === 'url-meta' ? JSON.stringify(resource.response) : '')
-      .reduce((acc, curr) => acc + curr.length, 0)
-      * 2
-      / 1024),
-  },
-  rootId: findType(homepageResource, 'page').id,
-  resources: zipByType(filterType(homepageResource, 'url-meta')),
-});
+const isMeta = (resource: Resource) => (
+  resource.type === 'metaRequest'
+);
+
+const isDebug = (resource: Resource) => (
+  resource.type === 'debug'
+)
 
 const homeRouteHandler = (config: Config): RouteHandler => (req, res, next) => {
   fetchHomepage(config)
-    .then(homepageResources => {
-      res.send(formatResponse(homepageResources))
+    .then(resources => {
+      res.send(resources.filter(res => !isMeta(res)))
       res.end();
-    });
+    })
+    .catch(({ message, stack }) => {
+      res.status(500),
+      res.send({ errorMessage: message, trace: stack })
+      res.end();
+    })
 };
 
 export default homeRouteHandler;
