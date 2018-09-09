9578ce69fecd3cbbe7700fc54dd5921549bf4079
Luke Kaalim <luke@kaal.im>
2018-09-10 03:28:36 +1000

    add staging ovvride for CMS

--- src/resources/content/api.js
+++ src/resources/content/api.js
@@ -5,6 +5,9 @@ export const getContentApiDomain = (config: Config) => {
   switch (config.environment) {
     case 'prod':
       return 'content.api.ninemsn.com.au';
+    case 'stag':
+      return 'content.api.ninemsn.com.au';
+    case 'uat':
     default:
       return 'content.api.uat.ninemsn.com.au';
   }
