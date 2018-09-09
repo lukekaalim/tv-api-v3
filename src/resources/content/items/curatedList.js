16a2b0516eb14687bbe1febc8e4869ced30e78a0
Luke Kaalim <luke@kaal.im>
2018-09-10 03:29:15 +1000

    update curated list parser

--- src/resources/content/items/curatedList.js
+++ src/resources/content/items/curatedList.js
@@ -1,39 +1,65 @@
 // @flow
-import uuid from 'uuid/v4';
-import fetchJSON from '../../../lib/fetchJson';
-
 import type { Config } from '../../../types';
-import type { ItemID, TemplateID } from '../types';
-import type { EpisodeID } from '../../video/types';
+import type { CMSItemID } from '../types';
+import type { VMSEpisodeID, VMSClipID, VMSTvSeriesID } from '../../video/types';
 import type { Resource }from '../../types';
+
+import buildCuratedClipListResource from '../buildCuratedClipListResource';
+import buildCuratedEpisodeListResource from '../buildCuratedEpisodeListResource';
+import buildCuratedTvSeriesListResource from '../buildCuratedTvSeriesListResource';
+import { metaRequest } from '../../meta/buildMetaResource';
 
 import { getContentApiDomain } from '../api';
 import fetchEpisode from '../../video/fetchEpisode';
+import fetchTvSeries from '../../video/fetchTvSeries';
+import fetchClip from '../../video/fetchClip';
+
+type EpisodeResourceResponse = { resource: 'episodes', id: VMSEpisodeID };
+type ClipsResourceResponse = { resource: 'clips', id: VMSClipID };
+type TvSeriesResourceResponse = { resource: 'tv-series', id: VMSTvSeriesID };
 
 export type CuratedListResponse = {
   templateId: 'd58e6cd8-5f60-40be-ba1d-76a9dad6a4b5',
-  id: ItemID,
-  items: Array<{ id: EpisodeID, resource: 'episodes' | 'tv-series' | 'clips' }>,
+  id: CMSItemID,
+  items: Array<any>, // Flow type is dumb and cant distinguish homogenous union arrays.
 };
 
 const flatten = (acc, curr) => [...acc, ...curr];
 
-const curatedList = (config: Config, curatedList: CuratedListResponse): Promise<Array<Resource>> => {
-  const itemsIds = curatedList.items.map(item => item.id);
-  return Promise
-    .all(curatedList.items.map((item) => {
-      switch (item.resource) {
-        case 'episodes':
-          return fetchEpisode(config, item.id);
-        default:
-          return [];
-      }
-    }))
-    .then(itemsResources => [
-      ...itemsResources.reduce(flatten, []),
-      { type: 'list', id: curatedList.id, itemsIds },
-    ]);
+const curatedList = async (config: Config, curatedListResponse: CuratedListResponse): Promise<Array<Resource>> => {
+  const listType = curatedListResponse.items[0] ? curatedListResponse.items[0].resource : null;
+  const isListHomogenous = curatedListResponse.items.every(item => item.resource === listType);
+  if (!isListHomogenous) {
+    return [];
+  }
+  switch (listType) {
+    case 'episodes': {
+      const episodeItems: Array<EpisodeResourceResponse> = curatedListResponse.items;
+      const episodesIds = episodeItems.map(item => item.id);
+      return [
+        ...(await Promise.all(episodesIds.map(id => fetchEpisode(config, id)))).reduce(flatten, []),
+        buildCuratedEpisodeListResource(curatedListResponse.id, episodesIds),
+      ];
+    }
+    case 'tv-series': {
+      const tvSeriesItems: Array<TvSeriesResourceResponse> = curatedListResponse.items;
+      const tvSeriesIds = tvSeriesItems.map(item => item.id);
+      return [
+        ...(await Promise.all(tvSeriesIds.map(id => fetchTvSeries(config, id)))).reduce(flatten, []),
+        buildCuratedTvSeriesListResource(curatedListResponse.id, tvSeriesIds),
+      ];
+    }
+    case 'clips': {
+      const clipsItems: Array<ClipsResourceResponse> = curatedListResponse.items;
+      const clipsIds = clipsItems.map(item => item.id);
+      return [
+        ...(await Promise.all(clipsIds.map(id => fetchClip(config, id)))).reduce(flatten, []),
+        buildCuratedClipListResource(curatedListResponse.id, clipsIds),
+      ];
+    }
+    default:
+      return [];
+  }
 };
 
 export default curatedList;
-
